422de5bbcd702db21b158a9816d5fa7b57ec3acd
Leonardo <leonardo_tsuji@hotmail.com>
2018-09-02 22:57:40 -0300

    Primeiro commit de país

---
+++ quartoExercicio/src/main/java/edu/utfpr/cp/sa/dao/CountryDao.java
@@ -0,0 +1,85 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.utfpr.cp.sa.dao;
+
+import edu.utfpr.cp.sa.entity.Country;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+
+/**
+ *
+ * @author leona
+ */
+public class CountryDao {
+    private final Connection conn;
+    
+    public CountryDao(){
+        this.conn = new DatabaseConnection().getConnection();
+    }
+    
+    public void insert(Country country){
+        String sql = "insert into country (countryName, acronym, phoneDigits) values(?,?,?)";
+        try{
+            PreparedStatement stmt = this.conn.prepareStatement(sql);
+            
+            stmt.setString(1, country.getName());
+            stmt.setString(2, country.getAcronym());
+            stmt.setInt(3, country.getPhoneDigits());
+            
+            stmt.execute();
+            stmt.close();
+        }catch(SQLException ex){
+            ex.printStackTrace();
+            throw new RuntimeException(ex);
+        }
+        
+    }
+    public void getListaCountry(){
+        String sql = "select * from country";
+        try{
+            PreparedStatement stmt = this.conn.prepareStatement(sql);
+            ResultSet rs = stmt.executeQuery();
+            while(rs.next()){
+                String name = rs.getString("countryName");
+                String acronym = rs.getString("acronym");
+                int phoneDigits = rs.getInt("phoneDigits");
+            }
+            rs.close();
+            stmt.close();
+        }catch(SQLException e){
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+    }
+    
+    public void update(Country country){
+        String sql = "update country set countryName = ?, acronym = ?, phoneDigits = ? where countryName = ?";
+        try{
+            PreparedStatement stmt = this.conn.prepareStatement(sql);
+            stmt.setString(1, country.getName());
+            stmt.setString(2, country.getAcronym());
+            stmt.setInt(3, country.getPhoneDigits());
+
+        }catch(SQLException ex){
+            ex.printStackTrace();
+            throw new RuntimeException(ex); 
+        }
+    }
+    
+    public void delete(Country country){
+        String sql = "delete from country where countryName = ?";
+        try{
+            PreparedStatement stmt = this.conn.prepareStatement(sql);
+            stmt.setString(1, country.getName());
+        }catch(SQLException ex){
+            ex.printStackTrace();
+            throw new RuntimeException(ex); 
+        }
+    }
+}
---
+++ quartoExercicio/src/main/java/edu/utfpr/cp/sa/dao/DatabaseConnection.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.utfpr.cp.sa.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author leona
+ */
+public class DatabaseConnection {
+    private static String databaseURL = "jdbc:mysql://localhost:3306/Atividade04";
+    private static String username = "root";
+    private static String password = "utfpr";
+    private static Connection conn;
+        
+    public static Connection getConnection(){
+        try(Connection conn = DriverManager.getConnection(databaseURL, username, password)){
+            if (conn != null) {
+                System.out.println("Connected");
+            }
+        }catch(SQLException ex){
+            ex.printStackTrace();
+        }
+        return conn;
+    }
+    
+    /*public Connection getConnection(){
+        try {
+            return DriverManager.getConnection(databaseURL, username, password);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }*/
+}
